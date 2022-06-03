db50ed778ada8c4d2239940d52e0b0cff61e3a19
Natnael Feleke <nfeleke568@gmail.com>
2022-06-03 16:31:32 +0300

    finishing landing page

--- src/components/landing/Stats.js
+++ src/components/landing/Stats.js
@@ -1,4 +1,4 @@
-import { Box, Container, Grid, Typography } from '@mui/material'
+import { Box, Container, Divider, Grid, Typography } from '@mui/material'
 import React from 'react'
 
 const style = {
@@ -42,25 +42,115 @@ const Stats = () => {
          ...style
      }}>
 <Container sx={{
-    pt: 8,pb: 8
+    pt: 8,pb: 8,
+    width: '100%',
+    mx: 'auto',
+    boxSizing: 'border-box',
+    display: 'block',
+    px: 2,
+    '@media screen and (min-width: 576px)': {
+      px: '1.5rem', mx: 'auto', maxWidth: '540px',
+      position: 'relative',
+    },
+    '@media screen and (min-width: 768px)': {
+      px: '1.5rem', mx: 'auto', maxWidth: '720px',
+      position: 'relative',
+    },
+    '@media screen and (min-width: 992px)': {
+     maxWidth: '960px',
+     position: 'relative',
+     px: '1.5rem',
+     mx: 'auto'
+    },
 }} >
-     <Grid container item xs={12} lg={9} sx={{ mx: "auto" }}>
-          <Grid item xs={12} md={4}>
+     <Grid container item xs={12} lg={9} sx={{ 
+       m: "0px auto", width: '100%', boxSizing: 'border-box',display: 'flex', flexFlow: 'row wrap', maxWidth:'100%', flexGrow: '0',
+      '@media screen and (min-width: 992px)': {
+        flexBasis: '75%',
+        maxWidth: '75%',
+        flexGrow: '0',
+      },
+      }}>
+          <Grid item xs={12} md={4} sx={{
+            m:0,
+            flexBasis: '100%',
+        maxWidth: '100%',
+        flexGrow: '0',
+        '@media screen and (min-width: 768px)': {
+          flexBasis: '33.3333%',
+          maxWidth: '33.3333%',
+          flexGrow: '0',
+        },
+        '@media screen and (min-width: 992px)': {
+          flexBasis: '33.3333%',
+          maxWidth: '33.3333%',
+          flexGrow: '0',
+        },
+          }}>
             <CounterCard
               count={300}
               title="Classes"
               description="Of “high-performing” level are led by a certified project manager"
             />
           </Grid>
-          <Grid item xs={12} md={4} display="flex">
+          <Grid item xs={12} md={4} sx={{
+            m:0,
+            flexBasis: '100%',
+        maxWidth: '100%',
+        flexGrow: '0',display: 'flex',
+        
+        '@media screen and (min-width: 768px)': {
+          flexBasis: '33.3333%',
+          maxWidth: '33.3333%',
+          flexGrow: '0',
+        },
+        '@media screen and (min-width: 992px)': {
+          flexBasis: '33.3333%',
+          maxWidth: '33.3333%',
+          flexGrow: '0',
+        },
+          }}>
+            <Divider orientation='vertical' sx={{
+              flexShrink: '0',opacity: '0.25',width: '0.0625rem',borderRight: 'none',borderBottom: 'none',m:0,background: 'rgb(52 71 103 / 25%)',
+              '@media screen and (min-width: 0px)': {
+                display: 'none'
+              },
+              '@media screen and (min-width: 768px)': {
+             display: 'block'
+              },
+            }} />
             <CounterCard
               count={1000}
               plus={true}
               title="Students"
               description="That meets quality standards required by our users"
             />
+              <Divider orientation='vertical'  sx={{
+               flexShrink: '0',opacity: '0.25',width: '0.0625rem',borderRight: 'none',borderBottom: 'none',m:'0px 1rem 0px 0px',background: 'rgb(52 71 103 / 25%)',
+               '@media screen and (min-width: 0px)': {
+                 display: 'none'
+               },
+               '@media screen and (min-width: 768px)': {
+              display: 'block'
+               },
+            }}/>
           </Grid>
-          <Grid item xs={12} md={4}>
+          <Grid item xs={12} md={4} sx={{
+            m:0,
+            flexBasis: '100%',
+        maxWidth: '100%',
+        flexGrow: '0',
+        '@media screen and (min-width: 768px)': {
+          flexBasis: '33.3333%',
+          maxWidth: '33.3333%',
+          flexGrow: '0',
+        },
+        '@media screen and (min-width: 992px)': {
+          flexBasis: '33.3333%',
+          maxWidth: '33.3333%',
+          flexGrow: '0',
+        },
+          }}>
             <CounterCard
               count={10}
               title="Courses"
